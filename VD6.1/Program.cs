6431ed59fb981cb986c24e9b9d8c67e15695838c
MaiTheLongDraco <128013309+MaiTheLongDraco@users.noreply.github.com>
2023-10-31 21:23:51 +0700

    Add project files.

---
+++ VD6.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int[] a = new int[4];
+        try
+        {
+            for (int i = 0; i <= a.Length; i++)
+            {
+                Console.Write("a[{0}]: ", i);
+                a[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        catch (IndexOutOfRangeException)
+        {
+            Console.WriteLine("Vuot qua pham vi mang");
+        }
+        catch (ArrayTypeMismatchException)
+        {
+            Console.WriteLine("Kieu du lieu nhap vao sai");
+        }
+        Console.ReadKey();
+        bool t = true;
+        try
+        {
+            char c = Convert.ToChar(t);
+        }
+        catch (InvalidCastException)
+        {
+            Console.WriteLine("Khong the ep kieu");
+        }
+    }
+    
+}
